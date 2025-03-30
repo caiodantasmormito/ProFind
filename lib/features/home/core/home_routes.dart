982c677be661d2c6636822f09c813f62b3cd41c7
Caio Dantas <caiodantas26@hotmail.com>
2025-03-30 20:28:02 -0300

    fix: routes

--- lib/features/home/core/home_routes.dart
+++ lib/features/home/core/home_routes.dart
@@ -1,12 +1,23 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:profind/features/home/presentation/pages/home_page.dart';
+import 'package:profind/features/service_providers/domain/usecases/get_service_providers_usecase.dart';
+import 'package:profind/features/service_providers/presentation/bloc/get_service_providers_bloc.dart';
 
-/*sealed class HomeRoutes {
+sealed class HomeRoutes {
   static final List<RouteBase> routes = [
     GoRoute(
-        path: HomePage.routeName,
-        builder: (context, state) {
-          return HomePage();
-        }),
+      path: HomePage.routeName,
+      builder: (context, state) => MultiBlocProvider(
+        providers: [
+          BlocProvider<GetServiceProvidersBloc>(
+            create: (context) => GetServiceProvidersBloc(
+              useCase: context.read<GetServiceProvidersUsecase>(),
+            ),
+          ),
+        ],
+        child: const HomePage(),
+      ),
+    ),
   ];
-}*/
+}
